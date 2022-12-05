b5ebf6b7a624d990c90b6891e307945744576e46
RubenHeyse <ruben.heyse@icloud.com>
2022-12-05 16:54:45 +0000

    bug fixes and included string.h

--- Code/include/main.h
+++ Code/include/main.h
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curses.h>
+#include <string.h>
+#include <ctype.h>
+
 #include "../include/rs232.h"
 #include "../include/serial.h"
 
@@ -9,10 +12,6 @@ extern int errno;
 
 #define bdrate 115200               /* 115200 baud */
 #define debugState 1
-
-void Wakeup_Robot(char *buffer);
-void SendCommands(char *buffer);
-void Initialise_Robot(char *buffer);
 
 #define grid_cell_width_mm  30
 #define grid_cell_height_mm 30
@@ -25,5 +24,18 @@ typedef struct CELL{
     int shape_local_origin[2];
 } CELL;
 
+void wakeup_robot(char *buffer);
+void SendCommands(char *buffer);
+void initialise_robot(char *buffer);
+int initialise_cells(CELL *cell_array);
+
+typedef struct SHAPE{
+    char name[20];
+    int instructions[10][10][10];
+} SHAPE;
+
 #define scaled_shape_width_mm   20   
 #define shape_point_width       16
+
+int parse_shape_file(char *shape_file_path, SHAPE *shape_array);
+int define_shapes();
